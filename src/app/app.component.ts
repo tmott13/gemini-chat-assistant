b1e5ba87c972b25811974881632b26899851d204
Tanya Mott <mott.tanya13@gmail.com>
2026-02-02 13:24:05 -0500

    updated angular to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { GeminiService } from './gemini.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   title = 'gemini-chat-assistant';
